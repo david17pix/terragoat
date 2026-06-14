804943b660ab51b9ac0d18747de8e92a6f9392db
david17pix <dantolinez@paloaltonetworks.com>
2026-06-14 14:11:21 -0500

    Update instance.tf

--- terraform/azure/instance.tf
+++ terraform/azure/instance.tf
@@ -5,7 +5,7 @@ resource random_string "password" {
   min_numeric = 1
   min_upper   = 1
 }
-
+#TESTE
 resource azurerm_linux_virtual_machine "linux_machine" {
   admin_username                  = "terragoat-linux"
   admin_password                  = random_string.password.result
