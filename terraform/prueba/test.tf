0d9286e46d2e98616a2e74685577081211835f20
david17pix <dantolinez@paloaltonetworks.com>
2026-06-14 14:57:48 -0500

    Update test.tf

--- terraform/prueba/test.tf
+++ terraform/prueba/test.tf
@@ -1,43 +1,128 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+}
 
-resource random_string "password" {
+provider "azurerm" {
+  features {}
+}
+
+# -------------------------
+# PASSWORD
+# -------------------------
+resource "random_string" "password" {
   length      = 16
   special     = false
   min_lower   = 1
   min_numeric = 1
   min_upper   = 1
 }
-#TESTE
-resource azurerm_linux_virtual_machine "linux_machine" {
+
+# -------------------------
+# RESOURCE GROUP (FALTABA)
+# -------------------------
+resource "azurerm_resource_group" "example" {
+  name     = "terragoat-rg"
+  location = var.location
+}
+
+# -------------------------
+# NETWORK
+# -------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terragoat-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "terragoat-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# -------------------------
+# NETWORK INTERFACES (FALTABAN)
+# -------------------------
+resource "azurerm_network_interface" "ni_linux" {
+  name                = "ni-linux"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "ni_win" {
+  name                = "ni-win"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# -------------------------
+# LINUX VM
+# -------------------------
+resource "azurerm_linux_virtual_machine" "linux_machine" {
+  name                = "terragoat-linux"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_F2"
+
   admin_username                  = "terragoat-linux"
   admin_password                  = random_string.password.result
-  location                        = var.location
-  name                            = "terragoat-linux"
-  network_interface_ids           = [azurerm_network_interface.ni_linux.id]
-  resource_group_name             = azurerm_resource_group.example.name
-  size                            = "Standard_F2"
   disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.ni_linux.id
+  ]
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-
-
 }
 
-resource azurerm_windows_virtual_machine "windows_machine" {
-  admin_password        = random_string.password.result
-  admin_username        = "tg-${var.environment}"
-  location              = var.location
-  name                  = "tg-win"
-  network_interface_ids = [azurerm_network_interface.ni_win.id]
-  resource_group_name   = azurerm_resource_group.example.name
-  size                  = "Standard_F2"
+# -------------------------
+# WINDOWS VM
+# -------------------------
+resource "azurerm_windows_virtual_machine" "windows_machine" {
+  name                = "terragoat-windows"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_F2"
+
+  admin_username = "tg-admin"
+  admin_password = random_string.password.result
+
+  network_interface_ids = [
+    azurerm_network_interface.ni_win.id
+  ]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -49,18 +134,4 @@ resource azurerm_windows_virtual_machine "windows_machine" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
-
-  tags = merge({
-    terragoat   = true
-    environment = var.environment
-    }, {
-    git_commit           = "de3bb777f967989c2c3332faea16cb614c55ccc9"
-    git_file             = "terraform/azure/instance.tf"
-    git_last_modified_at = "2020-06-17 15:48:15"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "41938587-bdfc-4234-b941-560f1ae7d6c0"
-  })
 }
