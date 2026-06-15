e22caa83a81e1a719997a8903f57d9ccfcfb25bc
david17pix <dantolinez@paloaltonetworks.com>
2026-06-15 11:20:28 -0500

    Update test.tf

--- terraform/prueba/test.tf
+++ terraform/prueba/test.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-}
 
 provider "azurerm" {
   features {}
