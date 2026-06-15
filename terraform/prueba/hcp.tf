13623a3ab195d8b86b1b394637bf51f812c71a37
david17pix <dantolinez@paloaltonetworks.com>
2026-06-15 11:19:14 -0500

    Update hcp.tf

--- terraform/prueba/hcp.tf
+++ terraform/prueba/hcp.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
+
 
 provider "google" {
   project = var.project_id
